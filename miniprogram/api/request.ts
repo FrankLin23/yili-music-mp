a957287af97c3c9942fcf9dfcd5dd6b008580429
bobbylin <linzhangsheng23@163.com>
2022-02-20 23:25:22 +0800

    first commit

---
+++ miniprogram/api/request.ts
@@ -0,0 +1,21 @@
+const baseUrl = "https://yili-music-1571394-1309336797.ap-shanghai.run.tcloudbase.com";
+
+export const get = (uri: string) => {
+    wx.showLoading({
+        title:'加载中'
+    })
+    return new Promise((resolve, reject) => {
+        wx.request({
+            url: baseUrl + uri,
+            method: "GET",
+            success: (res) => {
+                resolve(res.data);
+            },
+            fail: reject,
+            complete: () => {
+                wx.hideLoading();
+            }
+        });
+    })
+    
+}
